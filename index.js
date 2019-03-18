4347c0d5e85b264eac860e79c6b02660eb5ddc16
bmordan <bernardmordan@gmail.com>
2019-03-18 20:36:46 +0000

    string fun

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser = "Bernard"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = welcomeMessage.split(' ')[0] + ", " + currentUser.substring(0, 1).toUpperCase() + "!"
